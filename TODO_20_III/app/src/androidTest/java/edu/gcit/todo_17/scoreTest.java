4357f0954a15fd898f0461b9c8c02d051ec5443e
kezang-jpg <kezang-jpg>
2021-05-25 01:25:15 +0600

    todo20

---
+++ TODO_20_III/app/src/androidTest/java/edu/gcit/todo_17/scoreTest.java
@@ -0,0 +1,269 @@
+package edu.gcit.todo_17;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class scoreTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void scoreTest() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.increaseTeam1), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withId(R.id.increaseTeam1), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withId(R.id.increaseTeam1), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.score_1), withText("3"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        textView.check(matches(withText("1")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.score_1), withText("3"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        textView2.check(matches(withText("8")));
+
+        ViewInteraction appCompatImageButton4 = onView(
+                allOf(withId(R.id.decreaseTeam1), withContentDescription("Minus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton4.perform(click());
+
+        ViewInteraction appCompatImageButton5 = onView(
+                allOf(withId(R.id.decreaseTeam1), withContentDescription("Minus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton5.perform(click());
+
+        ViewInteraction appCompatImageButton6 = onView(
+                allOf(withId(R.id.increaseTeam1), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton6.perform(click());
+
+        ViewInteraction appCompatImageButton7 = onView(
+                allOf(withId(R.id.decreaseTeam1), withContentDescription("Minus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton7.perform(click());
+
+        ViewInteraction appCompatImageButton8 = onView(
+                allOf(withId(R.id.increaseTeam1), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton8.perform(click());
+
+        ViewInteraction appCompatImageButton9 = onView(
+                allOf(withId(R.id.increaseTeam1), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton9.perform(click());
+
+        ViewInteraction appCompatImageButton10 = onView(
+                allOf(withId(R.id.increaseTeam2), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton10.perform(click());
+
+        ViewInteraction appCompatImageButton11 = onView(
+                allOf(withId(R.id.increaseTeam2), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton11.perform(click());
+
+        ViewInteraction appCompatImageButton12 = onView(
+                allOf(withId(R.id.decreaseTeam2), withContentDescription("Minus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton12.perform(click());
+
+        ViewInteraction appCompatImageButton13 = onView(
+                allOf(withId(R.id.decreaseTeam2), withContentDescription("Minus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton13.perform(click());
+
+        ViewInteraction appCompatImageButton14 = onView(
+                allOf(withId(R.id.decreaseTeam2), withContentDescription("Minus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        appCompatImageButton14.perform(click());
+
+        ViewInteraction appCompatImageButton15 = onView(
+                allOf(withId(R.id.increaseTeam2), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton15.perform(click());
+
+        ViewInteraction appCompatImageButton16 = onView(
+                allOf(withId(R.id.increaseTeam2), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton16.perform(click());
+
+        ViewInteraction appCompatImageButton17 = onView(
+                allOf(withId(R.id.increaseTeam2), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton17.perform(click());
+
+        ViewInteraction appCompatImageButton18 = onView(
+                allOf(withId(R.id.increaseTeam2), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton18.perform(click());
+
+        ViewInteraction appCompatImageButton19 = onView(
+                allOf(withId(R.id.increaseTeam2), withContentDescription("Plus Button"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton19.perform(click());
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.score_2), withText("4"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
+                        isDisplayed()));
+        textView3.check(matches(withText("5")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
